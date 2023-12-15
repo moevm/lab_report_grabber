8af25df51542c74bc1563db76837cc5d40f2f7b1
MikelazDotPy <onextopplay@gmail.com>
2023-12-16 00:40:51 +0300

    parse github

---
+++ Work.py
@@ -0,0 +1,6 @@
+class Work:
+    def __init__(self, eng_name, ru_name, description, code):
+        self.eng_name = eng_name
+        self.ru_name = ru_name
+        self.description = description
+        self.code = code
