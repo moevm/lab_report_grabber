5f1912d092dbb26f6a6a54f97d422b00efa2748e
MikelazDotPy <onextopplay@gmail.com>
2023-12-16 00:36:31 +0300

    func for get students

---
+++ Student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, full_name, group, github, works):
+        self.full_name = full_name
+        self.group = group
+        self.github = github
+        self.works = works
+
