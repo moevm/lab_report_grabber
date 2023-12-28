a557fa1ba3b4ef0ae409453b29a78f33e4a5e586
Мухаммад Сабалиров <60626980+MikelazDotPy@users.noreply.github.com>
2023-12-29 00:51:35 +0300

    Delete Work.py

--- Work.py
+++
@@ -1,6 +0,0 @@
-class Work:
-    def __init__(self, eng_name, ru_name, description, code):
-        self.eng_name = eng_name
-        self.ru_name = ru_name
-        self.description = description
-        self.code = code
